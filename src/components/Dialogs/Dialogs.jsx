9d541b3099711c116a6189f981716afc289c3795
Misha Kyyanychenko <pepefrog4you@gmail.com>
2022-06-01 18:37:02 +0300

    add component DialogItem and Messages to Dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,32 @@
 import style from './Dialogs.module.css';
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
+
+
+
+
 
 const Dialogs = (props) =>{
 return(
-  <div>
-    dialogs
+  <div className={style.dialogs}>
+   <div className={style.dialogItems}>
+     <DialogItem name='Dima' id='1'/>
+     <DialogItem name='Masha' id='2'/>
+     <DialogItem name='Vanya' id='3'/>
+     <DialogItem name='Victor' id='4'/>
+     <DialogItem name='Carl' id='5'/>
+    </div>
+
+
+      <div className={style.messages}>
+        <Message message='Hello' />
+        <Message message='How are you?' />
+        <Message message='Fine' />
+        <Message message='Nice car!' /> 
+        <Message message='keeek' /> 
+        <Message message='Yo!!' />
+      </div>
+
   </div>
 )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,21 @@ import style from './Navbar.module.css'
 const Navbar = ()=> {
   return <nav className={`${style.nav} borderShadow`}> 
 <div className={style.item}>
-  <NavLink to='/profile' style ={({ isActive}) => ({color: isActive ? 'rgb(58 163 207)' : 'black'})}>Profile</NavLink>
+  <NavLink to='/profile' style ={({ isActive}) => ({color: isActive ? 'var(--mainBlue)' : 'black'})}>Profile</NavLink>
   </div>
 <div className={style.item}>
-  <NavLink to='/dialogs' style ={({ isActive}) => ({color: isActive ? 'rgb(58 163 207)' : 'black'})}>Messages</NavLink>
+  <NavLink to='/dialogs' style ={({ isActive}) => ({color: isActive ? 'var(--mainBlue)' : 'black'})}>Messages</NavLink>
   </div>
 <div className={style.item}>
-  <NavLink to='/news' style ={({ isActive}) => ({color: isActive ? 'rgb(58 163 207)' : 'black'})}>News</NavLink>
+  <NavLink to='/news' style ={({ isActive}) => ({color: isActive ? 'var(--mainBlue)' : 'black'})}>News</NavLink>
   </div>
 
 <div className={style.item}><a href='#'>Music</a></div>
 
 <div className={style.item}>
-  <NavLink to='/settings' style ={({ isActive}) => ({color: isActive ? 'rgb(58 163 207)' : 'black'})}>Settings</NavLink>
+  <NavLink to='/settings' style ={({ isActive}) => ({color: isActive ? 'var(--mainBlue)' : 'black'})}>Settings</NavLink>
   </div>
 </nav>
 }
-// 'rgb(58 163 207)'
+
 export default Navbar 
