741e9224b21a94e052fd0ecbf358f0a6d1e4b29a
Misha Kyyanychenko <pepefrog4you@gmail.com>
2022-06-08 11:25:45 +0300

    refactoring state.js to OOP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe, addPost, updateNewPostChange } from './redux/state';
+//import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './redux/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,13 +14,13 @@ let rerenderEntireTree = (state) => {
 
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostChange={updateNewPostChange} />
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostChange={store.updateNewPostChange.bind(store)} />
     </React.StrictMode>
   );
 }
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,54 +1,110 @@
-let rerenderEntireTree = () => {
+// let rerenderEntireTree = () => {
+// }
 
-}
+// /*data*/
+// let state = {
+//   profilePage: {
+//     posts: [
+//       { id: 1, message: 'Hello,world!', mileage: 1000, date: 2020 },
+//       { id: 2, message: 'Change oil in gear', mileage: 15000, date: 2021 },
+//       { id: 3, message: 'Install new car radio Blapunkt', mileage: 16500, date: 2022 },
+//     ],
+//     newPostText: '',
+//   },
+//   messagesPage: {
+//     dialogs: [
+//       { id: 1, name: 'Dima' },
+//       { id: 2, name: 'Masha' },
+//       { id: 3, name: 'Vanya' },
+//       { id: 4, name: 'Victor' },
+//       { id: 5, name: 'Carl' },
+//     ],
+//     messages: [
+//       { id: 1, message: 'Hello' },
+//       { id: 2, message: 'How are you?' },
+//       { id: 3, message: 'Fine' },
+//       { id: 4, message: 'Nice car' },
+//       { id: 5, message: 'Yo!!!' },
+//     ],
+//   },
 
-/*data*/
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hello,world!', mileage: 1000, date: 2020 },
-      { id: 2, message: 'Change oil in gear', mileage: 15000, date: 2021 },
-      { id: 3, message: 'Install new car radio Blapunkt', mileage: 16500, date: 2022 },
-    ],
-    newPostText: '',
+// }
+
+// export let addPost = () => {
+//   let newPost = {
+//     id: state.profilePage.posts.id + 1,
+//     message: state.profilePage.newPostText,
+//     mileage: 'Unknown',
+//     date: 'Unknown',
+//   }
+//   state.profilePage.posts.push(newPost);
+//   state.profilePage.newPostText = '';
+//   rerenderEntireTree(state);
+// };
+
+// export let updateNewPostChange = (changeText) => {
+//   state.profilePage.newPostText = changeText;
+//   rerenderEntireTree(state);
+// }
+
+// export let subscribe = (observer) => {
+//   rerenderEntireTree = observer;
+// }
+// export default state;
+
+
+
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hello,world!', mileage: 1000, date: 2020 },
+        { id: 2, message: 'Change oil in gear', mileage: 15000, date: 2021 },
+        { id: 3, message: 'Install new car radio Blapunkt', mileage: 16500, date: 2022 },
+      ],
+      newPostText: '',
+    },
+    messagesPage: {
+      dialogs: [
+        { id: 1, name: 'Dima' },
+        { id: 2, name: 'Masha' },
+        { id: 3, name: 'Vanya' },
+        { id: 4, name: 'Victor' },
+        { id: 5, name: 'Carl' },
+      ],
+      messages: [
+        { id: 1, message: 'Hello' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Fine' },
+        { id: 4, message: 'Nice car' },
+        { id: 5, message: 'Yo!!!' },
+      ],
+    },
+
   },
-  messagesPage: {
-    dialogs: [
-      { id: 1, name: 'Dima' },
-      { id: 2, name: 'Masha' },
-      { id: 3, name: 'Vanya' },
-      { id: 4, name: 'Victor' },
-      { id: 5, name: 'Carl' },
-    ],
-    messages: [
-      { id: 1, message: 'Hello' },
-      { id: 2, message: 'How are you?' },
-      { id: 3, message: 'Fine' },
-      { id: 4, message: 'Nice car' },
-      { id: 5, message: 'Yo!!!' },
-    ],
+  getState() {
+    return this._state
   },
-
-}
-
-export let addPost = () => {
-  let newPost = {
-    id: state.profilePage.posts.id + 1,
-    message: state.profilePage.newPostText,
-    mileage: 'Unknown',
-    date: 'Unknown',
+  addPost() {
+    let newPost = {
+      id: this._state.profilePage.posts.id + 1,
+      message: this._state.profilePage.newPostText,
+      mileage: 'Unknown',
+      date: 'Unknown',
+    }
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostChange(changeText) {
+    this._state.profilePage.newPostText = changeText;
+    this._callSubscriber(this._state);
+  },
+  _callSubscriber() {
+    console.log('subscribe (observer) is empty')
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   }
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
 };
-
-export let updateNewPostChange = (changeText) => {
-  state.profilePage.newPostText = changeText;
-  rerenderEntireTree(state);
-}
-
-export let subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-export default state;
+export default store;
